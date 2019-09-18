2cd101e879a8d36760446c111355487c9c111048
MinmaisElena <hyoeun.choi22@ycdsbk12.ca>
2019-09-18 08:54:17 -0400

    print examples

---
+++ Working/ClassExamples/1_1.py
@@ -0,0 +1,13 @@
+print("This is a print statement")
+print("What's woring with this statement?")
+print("How about this one?")
+print(5)
+print(3.14)
+print("Here are the ages of my three children:", 12, 7, 4)
+print(3.14, "is my favorite pi")
+colour = "black"
+print("His shoes are", colour)
+print("His pants are", colour)
+length = 16
+width = 20
+print("The dimensions of the yard are", length,"by", width)
