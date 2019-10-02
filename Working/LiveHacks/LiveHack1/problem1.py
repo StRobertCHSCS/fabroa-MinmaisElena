ed8dd2ceeb3a40d876d7a275162034f45b9389ae
MinmaisElena <hyoeun.choi22@ycdsbk12.ca>
2019-10-02 08:56:27 -0400

    problem1.py

---
+++ Working/LiveHacks/LiveHack1/problem1.py
@@ -0,0 +1,18 @@
+#get the temperature in celcius
+#compute the temperature in fahrenheit
+#output fahrenheit
+
+'''
+-------------------------------------------------------------------------------
+Name:		problem1.py
+Purpose:	Compute the temperature in farenherit
+
+Author:		Elena.C
+
+Created:	02/10/2019
+------------------------------------------------------------------------------
+'''
+
+celcius = int(input("What is the degree outside in celsius? : "))
+farenherit = (9/5) * celcius + 32
+print("The degree outside in farenherit is " + str(farenherit))
