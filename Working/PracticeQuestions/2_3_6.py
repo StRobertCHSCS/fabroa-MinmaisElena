38524592af66c372c62eabeed936c8332ad38c82
MinmaisElena <hyoeun.choi22@ycdsbk12.ca>
2019-10-22 09:24:35 -0400

    parctice

---
+++ Working/PracticeQuestions/2_3_6.py
@@ -0,0 +1,6 @@
+reservationName = "Shonda"
+name = input("Enter your name : ")
+if reservationName == name:
+    print("Right this way!")
+else:
+    print("Sorry, we don't have a reservation under that name.")
