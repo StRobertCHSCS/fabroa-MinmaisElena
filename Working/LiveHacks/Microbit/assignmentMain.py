b26db8c1c4975d5c74d0fa2b072fb8a5aca8026e
MinmaisElena <hyoeun.choi22@ycdsbk12.ca>
2019-10-25 08:54:46 -0400

    home

---
+++ Working/LiveHacks/Microbit/assignmentMain.py
@@ -0,0 +1,126 @@
+'''
+-------------------------------------------------------------------------------
+
+Name:       step_counter.py
+
+Purpose:    To count steps using microbit
+
+ 
+
+Author: choi.E
+
+ 
+
+Created:    24/10/2019
+
+------------------------------------------------------------------------------
+
+'''
+
+ 
+
+from microbit import *
+
+import music
+
+ 
+
+count = 0
+
+direction = 'North'
+
+Buzzer_pin = pin0
+
+LED_pin = pin1
+
+ 
+
+def display_count():
+
+    display.scroll(str(count))
+
+ 
+
+def increment_count():
+
+    count = count + 1
+
+ 
+
+def get_heading():
+
+    angle = compass.heading()
+
+    if (angle == 0):
+
+        direction = 'North'
+
+    elif (angle > 0 and angle < 90):
+
+        direction = 'Northeast'
+
+    elif (angle == 90):
+
+        direction = 'East'
+
+    elif (angle > 90 and angle < 180):
+
+        direction = 'Southeast'
+
+    elif (angle == 180):
+
+        direction = 'South'
+
+    #...
+
+    else:
+
+        direction = 'Northwest'
+
+    LED_pin.write_digital(1)
+
+    display.scorll(str(direction))
+
+    LED_pin.write_digital(0)
+
+ 
+
+def reset_count():
+
+    count = 0
+
+    music.play('f3:4', pin=Buzzer_pin)
+
+ 
+
+def button_clicks():
+
+    if(button_a.is_pressed()):
+
+        reset_count()
+
+    if(button_b.is_pressed()):
+
+        get_heading()
+
+ 
+
+def main():
+
+    while(True):
+
+        display_count()
+
+        if(accelerometer.is_gesture('shake')):
+
+            increment_count()
+
+        button_clicks()
+
+            
+
+main()
+
+ 
+
+
