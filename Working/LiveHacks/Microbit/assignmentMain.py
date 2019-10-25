d7667f7e7dd2853c8efc8c3144c7fb5f63d59559
MinmaisElena <hyoeun.choi22@ycdsbk12.ca>
2019-10-25 09:42:22 -0400

    fg

--- Working/LiveHacks/Microbit/assignmentMain.py
+++ Working/LiveHacks/Microbit/assignmentMain.py
@@ -1,4 +1,5 @@
 '''
+
 -------------------------------------------------------------------------------
 
 Name:       step_counter.py
@@ -22,6 +23,7 @@ import music
 count = 0
 direction = 'North'
 Buzzer_pin = pin0
+
 LED_pin = pin1
 
 
@@ -30,71 +32,75 @@ def display_count():
 
 
 def increment_count():
-    count = count + 1
+    global count
+
+
+count += 1
 
 
 def get_heading():
+    global LED_pin
     angle = compass.heading()
-
     if (angle == 0):
-
         direction = 'North'
-
     elif (angle > 0 and angle < 90):
-
         direction = 'Northeast'
-
     elif (angle == 90):
-
         direction = 'East'
-
     elif (angle > 90 and angle < 180):
-
         direction = 'Southeast'
-
     elif (angle == 180):
-
         direction = 'South'
-
-    #...
-
     else:
-
         direction = 'Northwest'
 
     LED_pin.write_digital(1)
-
-    display.scorll(str(direction))
-
+    display.scroll(str(direction))
     LED_pin.write_digital(0)
 
 
 def reset_count():
-    count = 0
-    music.play('f3:4', pin=Buzzer_pin)
+    global count
+
+
+global Buzzer_pin
+count = 0
+music.play('f3:1', pin=Buzzer_pin)
 
 
 def button_clicks():
-
     if(button_a.is_pressed()):
-
         reset_count()
-
     if(button_b.is_pressed()):
-
         get_heading()
 
 
 def main():
-
     while(True):
-
         display_count()
-
         if(accelerometer.is_gesture('shake')):
-
             increment_count()
+        button_clicks()    
 
+
+main()
+global Buzzer_pin
+count = 0
+music.play('f3:1', pin=Buzzer_pin)
+
+
+def button_clicks():
+    if(button_a.is_pressed()):
+        reset_count()
+    if(button_b.is_pressed()):
+        get_heading()
+
+
+def main():
+    while(True):
+        display_count()
+        if(accelerometer.is_gesture('shake')):
+            increment_count()
         button_clicks()
 
 
