d84f44c51426a56d1a15932072225e33d4c7f316
MinmaisElena <hyoeun.choi22@ycdsbk12.ca>
2019-11-18 09:15:08 -0500

    livehack2_problem1

--- Working/LiveHacks/LiveHack2/livehack2_problem1.py
+++ Working/LiveHacks/LiveHack2/livehack2_problem1.py
@@ -20,6 +20,7 @@ Created:    18/11/2019
 weight = float(input("Enter your weight (kg) : "))
 height = float(input("Enter your height (m) : "))
 bmi = weight / (height **2)
+
 if (bmi > 25.0):
     print("You are overweight.")
 elif (bmi <= 25.0 and bmi >= 18.5):
