c7cf4fd8b74bf1a6c550aeca7914bfc9baa49c06
MinmaisElena <55242767+MinmaisElena@users.noreply.github.com>
2020-01-23 09:47:21 -0500

    Update cpt.py

--- Working/cpt.py
+++ Working/cpt.py
@@ -1,15 +1,29 @@
+'''
+-------------------------------------------------------------------------------
+Name:		cpt.py
+Purpose:	Detecting toxic chemicals in the soil
+
+Author:	    Choi.H, Sedighi.R, Yu.Y
+
+Created:	date in 21/01/2020
+------------------------------------------------------------------------------
+'''
+
 from microbit import *
 import music
 import time
-Buzzer_pin = pin0
-MoistureSensor_pin = pin1
-LED_pin = pin2
+ 
+# defining pins
+buzzer_pin = pin0
+moisture_sensor_pin = pin1
+led_pin = pin2
+ 
 while True:
-    if MoistureSensor_pin.read_analog() < 50:
+    if moisture_sensor_pin.read_analog() < 50:
         music.play('b3:1', pin=Buzzer_pin)
-        LED_pin.write_digital(0)
+        led_pin.write_digital(0)
         time.sleep(0.1)
-        LED_pin.write_digital(1)
+        led_pin.write_digital(1)
         time.sleep(0.1)
     else:
-        LED_pin.write_digital(0)
+        led_pin.write_digital(0)
